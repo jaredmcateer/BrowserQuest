63cc76f32832cf4f81d86b0a89f19c341b26a811
Jared McAteer <jared@arterys.com>
2016-04-18 08:09:59 -0600

    Rough pass at indentation on client files

--- client/js/mobs.js
+++ client/js/mobs.js
@@ -1,160 +1,160 @@
 
 define(['mob', 'timer'], function(Mob, Timer) {
 
-    var Mobs = {
-        Rat: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.RAT);
-                this.moveSpeed = 350;
-                this.idleSpeed = 700;
-                this.shadowOffsetY = -2;
-                this.isAggressive = false;
-            }
-        }),
+  var Mobs = {
+    Rat: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.RAT);
+        this.moveSpeed = 350;
+        this.idleSpeed = 700;
+        this.shadowOffsetY = -2;
+        this.isAggressive = false;
+      }
+    }),
 
-        Skeleton: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.SKELETON);
-                this.moveSpeed = 350;
-                this.atkSpeed = 100;
-                this.idleSpeed = 800;
-                this.shadowOffsetY = 1;
-                this.setAttackRate(1300);
-            }
-        }),
+    Skeleton: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.SKELETON);
+        this.moveSpeed = 350;
+        this.atkSpeed = 100;
+        this.idleSpeed = 800;
+        this.shadowOffsetY = 1;
+        this.setAttackRate(1300);
+      }
+    }),
 
-        Skeleton2: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.SKELETON2);
-                this.moveSpeed = 200;
-                this.atkSpeed = 100;
-                this.idleSpeed = 800;
-                this.walkSpeed = 200;
-                this.shadowOffsetY = 1;
-                this.setAttackRate(1300);
-            }
-        }),
+    Skeleton2: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.SKELETON2);
+        this.moveSpeed = 200;
+        this.atkSpeed = 100;
+        this.idleSpeed = 800;
+        this.walkSpeed = 200;
+        this.shadowOffsetY = 1;
+        this.setAttackRate(1300);
+      }
+    }),
 
-        Spectre: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.SPECTRE);
-                this.moveSpeed = 150;
-                this.atkSpeed = 50;
-                this.idleSpeed = 200;
-                this.walkSpeed = 200;
-                this.shadowOffsetY = 1;
-                this.setAttackRate(900);
-            }
-        }),
-        
-        Deathknight: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.DEATHKNIGHT);
-                this.atkSpeed = 50;
-        		this.moveSpeed = 220;
-        		this.walkSpeed = 100;
-        		this.idleSpeed = 450;
-        		this.setAttackRate(800);
-        		this.aggroRange = 3;
-            },
-            
-            idle: function(orientation) {
-                if(!this.hasTarget()) {
-                    this._super(Types.Orientations.DOWN);
-                } else {
-                    this._super(orientation);
-                }
-            }
-        }),
+    Spectre: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.SPECTRE);
+        this.moveSpeed = 150;
+        this.atkSpeed = 50;
+        this.idleSpeed = 200;
+        this.walkSpeed = 200;
+        this.shadowOffsetY = 1;
+        this.setAttackRate(900);
+      }
+    }),
 
-        Goblin: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.GOBLIN);
-                this.moveSpeed = 150;
-                this.atkSpeed = 60;
-                this.idleSpeed = 600;
-                this.setAttackRate(700);
-            }
-        }),
+    Deathknight: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.DEATHKNIGHT);
+        this.atkSpeed = 50;
+        this.moveSpeed = 220;
+        this.walkSpeed = 100;
+        this.idleSpeed = 450;
+        this.setAttackRate(800);
+        this.aggroRange = 3;
+      },
 
-        Ogre: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.OGRE);
-                this.moveSpeed = 300;
-                this.atkSpeed = 100;
-                this.idleSpeed = 600;
-            }
-        }),
+      idle: function(orientation) {
+        if(!this.hasTarget()) {
+          this._super(Types.Orientations.DOWN);
+        } else {
+          this._super(orientation);
+        }
+      }
+    }),
 
-        Crab: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.CRAB);
-                this.moveSpeed = 200;
-                this.atkSpeed = 40;
-                this.idleSpeed = 500;
-            }
-        }),
+    Goblin: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.GOBLIN);
+        this.moveSpeed = 150;
+        this.atkSpeed = 60;
+        this.idleSpeed = 600;
+        this.setAttackRate(700);
+      }
+    }),
 
-        Snake: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.SNAKE);
-                this.moveSpeed = 200;
-                this.atkSpeed = 40;
-                this.idleSpeed = 250;
-                this.walkSpeed = 100;
-                this.shadowOffsetY = -4;
-            }
-        }),
+    Ogre: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.OGRE);
+        this.moveSpeed = 300;
+        this.atkSpeed = 100;
+        this.idleSpeed = 600;
+      }
+    }),
 
-        Eye: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.EYE);
-                this.moveSpeed = 200;
-                this.atkSpeed = 40;
-                this.idleSpeed = 50;
-            }
-        }),
+    Crab: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.CRAB);
+        this.moveSpeed = 200;
+        this.atkSpeed = 40;
+        this.idleSpeed = 500;
+      }
+    }),
 
-        Bat: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.BAT);
-                this.moveSpeed = 120;
-                this.atkSpeed = 90;
-                this.idleSpeed = 90;
-                this.walkSpeed = 85;
-                this.isAggressive = false;
-            }
-        }),
+    Snake: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.SNAKE);
+        this.moveSpeed = 200;
+        this.atkSpeed = 40;
+        this.idleSpeed = 250;
+        this.walkSpeed = 100;
+        this.shadowOffsetY = -4;
+      }
+    }),
 
-        Wizard: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.WIZARD);
-                this.moveSpeed = 200;
-                this.atkSpeed = 100;
-                this.idleSpeed = 150;
-            }
-        }),
+    Eye: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.EYE);
+        this.moveSpeed = 200;
+        this.atkSpeed = 40;
+        this.idleSpeed = 50;
+      }
+    }),
 
-        Boss: Mob.extend({
-            init: function(id) {
-                this._super(id, Types.Entities.BOSS);
-                this.moveSpeed = 300;
-                this.atkSpeed = 50;
-                this.idleSpeed = 400;
-                this.atkRate = 2000;
-                this.attackCooldown = new Timer(this.atkRate);
-        		this.aggroRange = 3;
-            },
-            
-            idle: function(orientation) {
-                if(!this.hasTarget()) {
-                    this._super(Types.Orientations.DOWN);
-                } else {
-                    this._super(orientation);
-                }
-            }
-        })
-    };
+    Bat: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.BAT);
+        this.moveSpeed = 120;
+        this.atkSpeed = 90;
+        this.idleSpeed = 90;
+        this.walkSpeed = 85;
+        this.isAggressive = false;
+      }
+    }),
 
-    return Mobs;
+    Wizard: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.WIZARD);
+        this.moveSpeed = 200;
+        this.atkSpeed = 100;
+        this.idleSpeed = 150;
+      }
+    }),
+
+    Boss: Mob.extend({
+      init: function(id) {
+        this._super(id, Types.Entities.BOSS);
+        this.moveSpeed = 300;
+        this.atkSpeed = 50;
+        this.idleSpeed = 400;
+        this.atkRate = 2000;
+        this.attackCooldown = new Timer(this.atkRate);
+        this.aggroRange = 3;
+      },
+
+      idle: function(orientation) {
+        if(!this.hasTarget()) {
+          this._super(Types.Orientations.DOWN);
+        } else {
+          this._super(orientation);
+        }
+      }
+    })
+  };
+
+  return Mobs;
 });
